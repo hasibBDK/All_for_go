7ac10b190e8dcb07100ee67a5ec40f49560c0028
hasibBDK <hn91690434@gmail.com>
2025-02-28 12:57:26 +0600

    my new go code

--- clouser/main.go
+++ clouser/main.go
@@ -4,6 +4,29 @@ import "fmt"
 const a = 10
 var p = 100
 
-func outsidr() {
-	
+func outsidr() func() {
+	money := 100
+	age := 20
+	fmt.Println("age: ", age)
+	show := func() {
+	money = money +a+ p
+    fmt.Println(money)
+	}
+	return show
+}
+
+func call() {
+	incr := outsidr()
+	incr()
+	incr()
+	incr2 := outsidr()
+	incr2()
+	incr2()
+
+}
+func main() {
+	call()
+}
+func init() {
+	fmt.Println("Today know about clouser")
 }
